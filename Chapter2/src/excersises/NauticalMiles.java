14b69e9bd3a407e7de2009e17093603cdae0fff5
HannahSwigart6 <hannahdeanswigart@gmail.com>
2017-09-07 15:18:55 -0500

    Assessment

---
+++ Chapter2/src/excersises/NauticalMiles.java
@@ -0,0 +1,25 @@
+package excersises;
+
+import java.util.Scanner;
+
+public class NauticalMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final double  kilometers = 1.852;
+		final double  miles = 1.150779;
+		double nauticalMile, numberKilometers, numberMiles;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(" Enter a number of nautical miles >>");
+		nauticalMile = input.nextInt();
+		
+		numberKilometers = kilometers * nauticalMile; 
+		numberMiles = miles * nauticalMile;
+		
+		System.
+		
+	}
+
+}
